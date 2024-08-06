abbd1d086f58c9d98b9e4cef9f74c25d18ecc71a
WhereSauce19 <dasindusirimanna668@gmail.com>
2024-08-06 18:17:42 +0530

    Update Prime_number_checker.py

--- Python/Prime_number_checker.py
+++ Python/Prime_number_checker.py
@@ -4,24 +4,26 @@ while True:
     # Inputting number
     n = input("Input number: ")
     # Checking if number is above 0 and is a digit
-    if not(n.isdigit()) or (n == 0):
+    if not(n.isdigit()) or (n == '0'):
         print("Please enter an integer.")
         continue        
-
-    n = int(n)            
-    prime = True
-    # Looping every number until the square root of the entered number 
-    # (Since every factor of a number is lesser than or qual to the square root)
-    for i in range(2, int(sqrt(n)+1)):
-        # Checking if the looped number divides the entered number without remainders
-        if n % i == 0:
-            prime = False
-            break
-        else:
-            continue
-    # Outputting whether entered number is prime or not
-    if prime:
-        print(f"{n} is prime.")
+    elif n == '1':
+        print("1 is not prime")
     else:
-        print(f"{n} is not prime.")
+        n = int(n)            
+        prime = True
+        # Looping every number until the square root of the entered number 
+        # (Since every factor of a number is lesser than or qual to the square root)
+        for i in range(2, int(sqrt(n)+1)):
+            # Checking if the looped number divides the entered number without remainders
+            if n % i == 0:
+                prime = False
+                break
+            else:
+                continue
+        # Outputting whether entered number is prime or not
+        if prime:
+            print(f"{n} is prime.")
+        else:
+            print(f"{n} is not prime.")
     print()
