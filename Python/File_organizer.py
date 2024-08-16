cf6caea6557e639054f0000756f0276e93d058b4
WhereSauce19 <dasindusirimanna668@gmail.com>
2024-08-16 18:42:56 +0530

    Update File_organizer.py
    >Fixed errors.
    >Added more features.

--- Python/File_organizer.py
+++ Python/File_organizer.py
@@ -2,43 +2,40 @@ import os
 from shutil import move
 from tkinter import messagebox
 
-def extensions_folders():
-    dict = {}
-    with open("extensions and folders.txt", 'r') as file:
-        for line in file.readlines():
-            line = line.strip()
-            if not(line.startswith('#')):
-                ext,fold = line.split('|')
-                dict[ext] = fold
-    return dict
+# Getting the Documents directory of computer.
+text_files_path = "~\\Documents\\File Organizer(files)"
+text_files_path = os.path.expanduser(text_files_path)
+# Creating a folder for text files if it dose'nt exist.
+if not(os.path.exists(text_files_path)):
+    os.mkdir(text_files_path)
+# Creating extension and folder Dictionary.
+def read_ext_fold():
+    file_name = "ext_fold.txt"
+    ext_fold_path = os.path.join(text_files_path, file_name)
+    dict1 = {}
+    # Checking if file already exists.
+    try:
+        with open(ext_fold_path, 'r') as file:
+            for line in file.readlines():
+                line = line.strip()
+                if not(line.startswith('#')):
+                    ext, fold = line.split('|')
+                    dict1[ext] = fold
+    # Handling exception if file dose'nt exist.
+    except FileNotFoundError:
+        file = open(ext_fold_path, 'w')
+        file.close()
+        messagebox.showinfo("File Created", f"ext_fold.txt file created at {text_files_path}")
+    # Returning the created Dictionary .
+    return dict1
 
-def file_directories(dest_dir):
-    with open("file directories.txt", 'r') as file:
-        for line in file.readlines():
-            direc = ""
-            line = line.strip()
-            for char in line:
-                if char == "\\":
-                    char = char *2
-                direc += char
-            organize_files(direc, dest_dir)
+# Initializing read_ext_fold function.
+ext_fold = {}
+ext_fold = read_ext_fold()
 
 def organize_files(files_dir, dest_dir):
     # listing files directory.
     files = os.listdir(files_dir)
-    # Extensions and relevant folders.
-    ext_fold = {}
-    try:
-        # Reading extensions and folders from text file.
-        ext_fold = extensions_folders()
-    except FileNotFoundError:
-        # Handling file if it dosen't exist
-        file = open("extensions and folders.txt", 'w')
-        file.close()
-        messagebox.showinfo("File Created", f"extensions and folders.txt file created at {os.getcwd()}")
-        # Rereading the extensions and folder from text file.
-        ext_fold = extensions_folders()
-
     # Looping across files list.
     for file in files:
         name, ext = os.path.splitext(file)
@@ -69,13 +66,33 @@ def organize_files(files_dir, dest_dir):
                         counter += 1
                         continue
 
-try:
-    # Reading file directories from text file.
-    file_directories("C:\\Users\\sanit\\Videos")
-except FileNotFoundError:
-    # Handling file if it dosen't exist.
-    file = open("file directories.txt", 'w')
-    file.close()
-    messagebox.showinfo("File Created", f"file directories.txt file created at {os.getcwd()}")
-    # Rereading the file directories from text file.
-    file_directories("C:\\Users\\sanit\\Videos")
+# Reading file_directories from text file.
+def read_file_dir():
+    file_name = "file_dir.txt"
+    file_dir_path = os.path.join(text_files_path, file_name)
+    file_directory_list = []
+    # Checking if file already exists.
+    try:
+        with open(file_dir_path, 'r') as file:
+            for line in file.readlines():
+                file_directory = line.strip()
+                # Checking if file directory already exist.
+                try:
+                    file_directory_list.index(file_directory)
+                # Appending directory if it dose'nt exist.
+                except ValueError:
+                    file_directory_list.append(file_directory)
+    # Handling exception if file dose'nt exist.
+    except FileNotFoundError:
+        file = open(file_dir_path, 'w')
+        file.close()
+        messagebox.showinfo("File Created", f"file_dir.txt file created at {text_files_path}")
+    # Returning directories by a list.
+    return file_directory_list
+
+# Destination directory.
+dest_dir = "Destination directory goes here"
+# Looping over the directories list:
+for file_directory in read_file_dir():
+    # Organizing files.
+    organize_files(file_directory, dest_dir)
